a64c1f23aa7a624cd3a3a3bdf7b1a2c382501132
Abdullah Al-Tamjid <168060724+ampfibi1@users.noreply.github.com>
2026-07-24 12:51:47 +0600

    task 3 last 2 work

--- src/admin/task3/task3.controller.ts
+++ src/admin/task3/task3.controller.ts
@@ -1,18 +1,29 @@
-import { Body, Controller, Param, ParseIntPipe, Patch, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post } from "@nestjs/common";
 import { Task3Service } from "./task3.service";
 import { CreateUserDto } from "./dto/create-user.dto";
+import { UserInfo } from "./entities/userinfo.entity";
 
 @Controller('task3')
 export class Task3Controller{
     constructor(private readonly task3Service:Task3Service){}
 
     @Post('createUser')
-    create(@Body() crtUserDto:CreateUserDto){
+    create(@Body() crtUserDto:CreateUserDto):Promise<UserInfo>{
         return this.task3Service.createUser(crtUserDto);
     }
 
-    @Patch('updateCountry/:id')
-    updateCounty(@Param("id",ParseIntPipe) id:number,@Body("country") country:string){
+    @Get('updateCountry/:id')
+    updateCounty(@Param("id",ParseIntPipe) id:number,@Body("country") country:string):Promise<UserInfo|null>{
         return this.task3Service.updateCounty(id,country);
+    }
+    
+    @Get('findUByJDate/:date')
+    findByJDate(@Param('date') date:string):Promise<UserInfo[]>{
+        return this.task3Service.findByJDate(date);
+    }
+
+    @Get('findUnknownCounty')
+    findByDefaultCounty():Promise<UserInfo[]>{
+        return this.task3Service.findByDefaultCounty()
     }
 }
--- src/admin/task3/task3.service.ts
+++ src/admin/task3/task3.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { UserInfo } from "./entities/userinfo.entity";
-import { Repository } from "typeorm";
+import { Between, Repository } from "typeorm";
 import { CreateUserDto } from "./dto/create-user.dto";
 
 @Injectable()
 export class Task3Service{
     constructor(@InjectRepository(UserInfo) private readonly userRepo:Repository<UserInfo>){}
 
-    async createUser(createUser:CreateUserDto):Promise<CreateUserDto>{
+    async createUser(createUser:CreateUserDto):Promise<UserInfo>{
         return await this.userRepo.save(createUser);
     }
 
@@ -17,5 +17,13 @@ export class Task3Service{
         return await this.userRepo.findOne({where:{id:id}});
     }
 
-    
+    async findByJDate(date:string):Promise<UserInfo[]>{
+        return await this.userRepo.find({where:
+            {joiningDate:Between
+                (new Date(`${date}T00:00:00`),new Date(`${date}T23:59:59.999`))}});
+    }
+
+    async findByDefaultCounty():Promise<UserInfo[]>{
+        return await this.userRepo.find({where:{country:'Unknown'}});
+    }
 }
