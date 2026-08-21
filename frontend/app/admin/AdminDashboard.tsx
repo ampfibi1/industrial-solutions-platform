7322b0743a82cba116c6ee8e931ec64d69669c78
abdullahtamjid676@gmail.com <168060724+ampfibi1@users.noreply.github.com>
2026-08-21 23:33:04 +0600

    add props

---
+++ frontend/app/admin/AdminDashboard.tsx
@@ -0,0 +1,14 @@
+type AdminDashboardProps = {
+  id: string;
+};
+
+export default function AdminDashboard({
+  id,
+}: AdminDashboardProps) {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      <p>Admin ID: {id}</p>
+    </div>
+  );
+}
--- frontend/app/admin/[id]/page.tsx
+++ frontend/app/admin/[id]/page.tsx
@@ -1,11 +1,11 @@
-export default async function AdminPage({params}: {params:{ id: string }}) {
-    const { id } = await params;    
-    return (
-      <main className="flex min-h-screen items-center justify-center">
-        <div>
-          <h1 className="text-3xl font-bold">Admin Dashboard</h1>
-          <p className="mt-2 text-gray-500">Admin ID: {id}</p>
-        </div>
-      </main>
-    );
+import AdminDashboard from "../AdminDashboard";
+
+export default async function AdminPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  return <AdminDashboard id={id} />;
 }
