83803cdde0b396a8bbe46b2f167a64f913baaca8
abdullahtamjid676@gmail.com <168060724+ampfibi1@users.noreply.github.com>
2026-09-11 00:31:17 +0600

    solve edit issue

--- frontend/components/admin/users/CreateUser.tsx
+++ frontend/components/admin/users/CreateUser.tsx
@@ -5,7 +5,8 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState, type SubmitEvent } from "react";
 import { z } from "zod";
 
-const signUpSchema = z.object({
+const signUpSchema = z
+  .object({
     name: z.string().min(2, "Name must be at least 2 characters"),
     email: z.email("Invalid email"),
     phone: z.string().min(11, "Phone number must be at least 11 characters"),
@@ -39,7 +40,13 @@ type User = {
   };
 };
 
-export default function CreateEditUserForm({user,onCancel}: {user?: User;onCancel?: () => void;}) {
+export default function CreateEditUserForm({
+  user,
+  onCancel,
+}: {
+  user?: User;
+  onCancel?: () => void;
+}) {
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const [companies, setCompanies] = useState<Company[]>([]);
@@ -50,15 +57,18 @@ export default function CreateEditUserForm({user,onCancel}: {user?: User;onCance
   useEffect(() => {
     async function getCompanies() {
       try {
-        const response = await axios.get("http://localhost:3000/admin/companies");
+        const response = await axios.get(
+          "http://localhost:3000/admin/companies"
+        );
+
         setCompanies(response.data);
       } catch (err) {
         console.error("Failed to load companies", err);
       }
     }
+
     getCompanies();
-  }, []);//while i'm using [] empty array that't mean the useEffect only work in first rendaring 
-  //without this [] , calls with every rendering 
+  }, []);
 
   async function handleSubmit(e: SubmitEvent<HTMLFormElement>) {
     e.preventDefault();
@@ -69,13 +79,13 @@ export default function CreateEditUserForm({user,onCancel}: {user?: User;onCance
     const formData = new FormData(e.currentTarget);
 
     const result = signUpSchema.safeParse({
-      name: formData.get("name"),
-      email: formData.get("email"),
-      phone: formData.get("phone"),
-      password: formData.get("password"),
-      confirmPassword: formData.get("confirmPassword"),
-      role: formData.get("role"),
-      companyId: formData.get("companyId"),
+      name: String(formData.get("name") ?? ""),
+      email: String(formData.get("email") ?? ""),
+      phone: String(formData.get("phone") ?? ""),
+      password: String(formData.get("password") ?? ""),
+      confirmPassword: String(formData.get("confirmPassword") ?? ""),
+      role: String(formData.get("role") ?? ""),
+      companyId: String(formData.get("companyId") ?? ""),
     });
 
     if (!result.success) {
@@ -86,18 +96,23 @@ export default function CreateEditUserForm({user,onCancel}: {user?: User;onCance
 
     try {
       if (isEdit) {
-        await axios.put(
+        await axios.patch(
           `http://localhost:3000/admin/users/${user.id}`,
           {
             name: result.data.name,
             email: result.data.email,
             phone: result.data.phone,
             role: result.data.role,
-            companyId: result.data.companyId === ""? null: Number(result.data.companyId),
+            companyId:
+              result.data.companyId === ""
+                ? null
+                : Number(result.data.companyId),
           }
         );
+
         alert("User updated successfully");
-        onCancel?.();//call onCancel if it exists.
+
+        onCancel?.();
       } else {
         await axios.post(
           "http://localhost:3000/admin/createUser",
@@ -107,17 +122,27 @@ export default function CreateEditUserForm({user,onCancel}: {user?: User;onCance
             phone: result.data.phone,
             password: result.data.password,
             role: result.data.role,
-            companyId: result.data.companyId === "" ? null : Number(result.data.companyId),
+            companyId:
+              result.data.companyId === ""
+                ? null
+                : Number(result.data.companyId),
           }
         );
+
         alert("User created successfully");
       }
 
       router.refresh();
     } catch (err: unknown) {
       if (axios.isAxiosError(err)) {
-        const message = err.response?.data?.message || "Request failed";
-        setError(Array.isArray(message) ? message[0] : message);
+        const message =
+          err.response?.data?.message || "Request failed";
+
+        setError(
+          Array.isArray(message)
+            ? message[0]
+            : message
+        );
       } else {
         setError("Something went wrong. Please try again.");
       }
@@ -127,52 +152,114 @@ export default function CreateEditUserForm({user,onCancel}: {user?: User;onCance
   }
 
   return (
-    <form onSubmit={handleSubmit} className="border rounded-xl p-6 space-y-4">
-      <h2 className="text-xl font-semibold"> {isEdit ? "Edit User" : "Create User"}</h2>
-      {error && (<p className="text-sm text-red-500"> {error} </p>)}
+    <form
+      onSubmit={handleSubmit}
+      className="border rounded-xl p-6 space-y-4"
+    >
+      <h2 className="text-xl font-semibold">
+        {isEdit ? "Edit User" : "Create User"}
+      </h2>
+
+      {error && (
+        <p className="text-sm text-red-500">
+          {error}
+        </p>
+      )}
 
       <div className="grid md:grid-cols-2 gap-4">
-        <input className="border rounded-lg p-2" placeholder="Name" name="name" type="text" defaultValue={user?.name ?? ""} />
-        <input className="border rounded-lg p-2" placeholder="Email" type="email" name="email" defaultValue={user?.email ?? ""} />
+        <input
+          className="border rounded-lg p-2"
+          placeholder="Name"
+          name="name"
+          type="text"
+          defaultValue={user?.name ?? ""}
+        />
+
+        <input
+          className="border rounded-lg p-2"
+          placeholder="Email"
+          type="email"
+          name="email"
+          defaultValue={user?.email ?? ""}
+        />
 
         {!isEdit && (
           <>
-            <input className="border rounded-lg p-2" placeholder="Password" type="password" name="password" />
-            <input className="border rounded-lg p-2" placeholder="Confirm Password" type="password" name="confirmPassword" />
+            <input
+              className="border rounded-lg p-2"
+              placeholder="Password"
+              type="password"
+              name="password"
+            />
+
+            <input
+              className="border rounded-lg p-2"
+              placeholder="Confirm Password"
+              type="password"
+              name="confirmPassword"
+            />
           </>
         )}
 
-        <input className="border rounded-lg p-2" placeholder="Phone" type="tel" name="phone" defaultValue={user?.phone ?? ""}/>
+        <input
+          className="border rounded-lg p-2"
+          placeholder="Phone"
+          type="tel"
+          name="phone"
+          defaultValue={user?.phone ?? ""}
+        />
 
-        <select className="border rounded-lg p-2" name="role" defaultValue={user?.role ?? "ADMIN"}>
+        <select
+          className="border rounded-lg p-2"
+          name="role"
+          defaultValue={user?.role ?? "ADMIN"}
+        >
           <option value="ADMIN">ADMIN</option>
-          <option value="SALES_EXECUTIVE">SALES EXECUTIVE</option>
+          <option value="SALES_EXECUTIVE">
+            SALES EXECUTIVE
+          </option>
           <option value="ENGINEER">ENGINEER</option>
         </select>
 
-        <select className="border rounded-lg p-2" name="companyId" defaultValue={user?.company?.id ?? ""} >
+        <select
+          className="border rounded-lg p-2"
+          name="companyId"
+          defaultValue={user?.company?.id ?? ""}
+        >
           <option value="">No Company</option>
+
           {companies.map((company) => (
-            <option key={company.id} value={company.id}>
+            <option
+              key={company.id}
+              value={company.id}
+            >
               {company.name}
             </option>
           ))}
         </select>
-
       </div>
 
       <div className="space-x-2">
-
-        <button type="submit" className="bg-black text-white px-5 py-2 rounded-lg" >
-          {loading ? "Saving..." : isEdit? "Update User": "Create User"}
+        <button
+          type="submit"
+          className="bg-black text-white px-5 py-2 rounded-lg"
+        >
+          {loading
+            ? "Saving..."
+            : isEdit
+            ? "Update User"
+            : "Create User"}
         </button>
 
         {isEdit && (
-          <button type="button" onClick={onCancel} className="px-5 py-2 rounded-lg border">
+          <button
+            type="button"
+            onClick={onCancel}
+            className="px-5 py-2 rounded-lg border"
+          >
             Cancel
           </button>
         )}
-
       </div>
     </form>
   );
